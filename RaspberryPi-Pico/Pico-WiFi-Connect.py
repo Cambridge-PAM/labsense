d3faaa8ca0d673af9d9645e8354593b1f9a9a2c0
Dr Takashi Lawson <31657144+takashilawson@users.noreply.github.com>
2023-11-15 15:30:34 +0000

    Set up of MicroPython Project

---
+++ RaspberryPi-Pico/Pico-WiFi-Connect.py
@@ -0,0 +1,24 @@
+import network
+from utime import sleep
+import machine
+
+ssid = 'DESKTOP-S2IAEKH 9692'
+password = '4@7t588L'
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    return ip
+
+
+try:
+    ip = connect()
+except KeyboardInterrupt:
+    machine.reset()
