7a2d3e7c0f609a19fd74e9e00fd0924b6fa3fbd3
Dr Takashi Lawson <31657144+takashilawson@users.noreply.github.com>
2023-11-24 14:59:53 +0000

    Updated regular temp schedule

--- RaspberryPi-4/Azure-Stream-Temp-Schedule.py
+++ RaspberryPi-4/Azure-Stream-Temp-Schedule.py
@@ -10,22 +10,27 @@ async def main():
     # Fetch the connection string from an environment variable
     conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
 
+    # Create instance of the device client using the authentication provider
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+    await device_client.connect()
+
     # Send a message
-    def send_temp():
-        device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
-        await device_client.connect()
-        cpu=CPUTemperature()
-        time_send=datetime.now()
-        cpu_msg=str({"Temp":cpu.temperature, "Time":time_send.strftime('%Y-%m-%d %H:%M:%S')})
-        await device_client.send_message(cpu_msg)
-        print("Message successfully sent!")
-        await device_client.shutdown()
-    
-    schedule.every(5).minutes.do(send_temp)
+    cpu=CPUTemperature()
+    time_send=datetime.now()
+    cpu_msg=str({"Temp":cpu.temperature, "Time":time_send.strftime('%Y-%m-%d %H:%M:%S')})
+    await device_client.send_message(cpu_msg)
+    print("Message successfully sent!")
+
+    await device_client.shutdown()
+
+def job():
+    print("Job started")
+    asyncio.run(main())
+    print("job finished")
+
+if __name__ == "__main__":
+    schedule.every(1).minutes.do(job)
 
     while True:
         schedule.run_pending()
-        time.sleep(30)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+        time.sleep(1)
