67fc0b1abb4aaf8a631522957548c55b7b7a1960
Dr Takashi Lawson <31657144+takashilawson@users.noreply.github.com>
2023-11-24 12:14:51 +0000

    Update to Azure Stream and Raspberry Pi 4 code

---
+++ RaspberryPi-4/Azure-Stream-Test.py
@@ -0,0 +1,28 @@
+import os
+import asyncio
+from azure.iot.device.aio import IoTHubDeviceClient
+from gpiozero import CPUTemperature
+
+async def main():
+    # Fetch the connection string from an environment variable
+    conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+
+    # Create instance of the device client using the authentication provider
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+
+    # Connect the device client.
+    await device_client.connect()
+
+    # Send a single message
+    print("Sending message...")
+    cpu=CPUTemperature()
+    cpu_msg=str({"Temp":cpu.temperature})
+    await device_client.send_message(cpu_msg)
+    print("Message successfully sent!")
+
+    # finally, shut down the client
+    await device_client.shutdown()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
