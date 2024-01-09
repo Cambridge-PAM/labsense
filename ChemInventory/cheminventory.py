e5eab746a390792bbff57bbefabe98c8d347f566
OddMothball <michael.j.bennison@gmail.com>
2024-01-09 14:57:47 +0000

    new file:   ChemInventory/authtoken.py new file:   ChemInventory/cheminventory.py new file:   ChemInventory/ci_dict.py new file:   ChemInventory/dict.py new file:   ChemInventory/ghs.py new file:   ChemInventory/ghs_dict.py

---
+++ ChemInventory/cheminventory.py
@@ -0,0 +1,38 @@
+import requests as req
+import pandas as pd
+from authtoken import authtoken
+
+#Initial info request for account information, required to obtain Inventory ID for later use
+#TODO Move "authtoken" to secrets.py and link to prevent direct reveal.
+ci_info = req.post("https://app.cheminventory.net/api/general/getdetails", 
+                        json = {"authtoken": authtoken})
+ci_info_raw = ci_info.json()
+ci_info_df = pd.DataFrame(ci_info_raw ['data'])
+ci_info_df_compact = ci_info_df.iloc[[4,5,3], 0]
+print(ci_info_df_compact)
+
+#Request to list all physical locations in ChemInventory records
+ci_locs = req.post("https://app.cheminventory.net/api/location/load", 
+                        json = {"authtoken": authtoken})
+ci_locs_raw = ci_locs.json()
+ci_locs_df = pd.DataFrame(ci_locs_raw ['data']) 
+print(ci_locs_df)
+
+#Example query, using Inventory ID obtained from ci_info
+#TODO Directly link "inventory" to result from ci_info, to allow for simple user & inventory change.
+ci = req.post("https://app.cheminventory.net/api/search/execute",
+                   json = {"authtoken": authtoken,
+                           "inventory": 873,
+                           "type": "cas",
+                           "contents": "75-09-2"})
+ci_json_raw = ci.json()
+ci_json_data = pd.json_normalize(ci_json_raw ['data']['containers'])
+
+ci_df = pd.DataFrame(ci_json_data)
+print(ci_df)
+
+ci_df_real = ci_df.loc[ci_df["location"]!=527895]
+print(ci_df_real)
+
+
+print(ci_df.columns)
---
+++ RaspberryPi-4/Azure-Stream-Test copy.py
@@ -0,0 +1,54 @@
+import os
+import asyncio
+from datetime import datetime
+from azure.iot.device.aio import IoTHubDeviceClient
+from gpiozero import CPUTemperature
+
+import requests as req
+import json
+import pandas as pd
+from authtoken import authtoken
+
+
+
+async def main():
+    #Example search using "Red" environmental hazards
+    ghs = req.post("https://app.cheminventory.net/api/search/execute",
+                    json = {"authtoken": authtoken,
+                            "inventory": 873,
+                            "type": "ghs",
+                            "contents": {
+                                    "searchtype": "or",
+                                    "items": ["H400", "H401", "H410", "H411", "H412", "H420", "H441"]}})
+    ghs_json_raw = ghs.json()
+    ghs_json_data = pd.json_normalize(ghs_json_raw ['data']['containers'])
+
+    ghs_df = pd.DataFrame(ghs_json_data)
+    
+
+    ghs_df_quant = ghs_df.iloc[:, [1, 3, 4, 5, 6, 7]]
+   
+    
+    # Fetch the connection string from an environment variable
+    conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+
+    # Create instance of the device client using the authentication provider
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+
+    # Connect the device client.
+    await device_client.connect()
+
+    # Send a single message
+    print("Sending message...")
+    cpu=CPUTemperature()
+    time_send=datetime.now()
+    cpu_msg=str({"GHS":ghs_df_quant, "Time":time_send.strftime('%Y-%m-%d %H:%M:%S')})
+    await device_client.send_message(cpu_msg)
+    print("Message successfully sent!")
+
+    # finally, shut down the client
+    await device_client.shutdown()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
