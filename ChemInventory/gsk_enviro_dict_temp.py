7168352761cc13c9c08b0d3d608aff041a41d46b
OddMothball <michael.j.bennison@gmail.com>
2024-01-16 18:40:40 +0000

    Corrected CAS for Hexane

---
+++ ChemInventory/gsk_enviro_dict_temp.py
@@ -0,0 +1,70 @@
+gsk_composite_red = {"Tetrahydrofurfuryl alcohol":"97-99-4",
+                     "2-Methoxyethanol":"109-86-4",
+                     "Methyl formate":"107-31-3",
+                     "Pyridine":"110-86-1",
+                     "Benzene":"71-43-2",
+                     "Methylcyclopentane":"96-37-7",
+                     "Pentane":"109-66-0",
+                     "2-Methylpentane":"107-83-5",
+                     "Hexane":"110-54-3",
+                     "Petroleum spirit":"8032-32-4",
+                     "t-Butylmethyl ether":"1634-04-4",
+                     "Diisopropyl ether":"108-20-3",
+                     "Dimethoxymethane":"109-87-5",
+                     "Tetrahydrofuran":"109-99-9",
+                     "Bis(2-methoxyethyl) ether":"111-96-6",
+                     "1,4-Dioxane":"123-91-1",
+                     "Diethyl ether":"60-29-7",
+                     "1,2-Dimethoxyethane":"110-71-4",
+                     "Dimethyl ether":"115-10-6",
+                     "Sulfolane":"126-33-0",
+                     "Formamide":"75-12-7",
+                     "N-Methyl pyrrolidone":"872-50-4",
+                     "Dimethylacetamide":"127-19-5",
+                     "N-Ethylpyrrolidone":"2687-91-4",
+                     "N-Methylformamide":"123-39-7",
+                     "Dimethylformamide":"68-12-2",
+                     "Tetramethylurea":"632-22-4",
+                     "Carbon disulfide":"75-15-0",
+                     "2,2,2-Trifluoroethanol":"75-89-8",
+                     "Furfural":"98-01-1"}
+
+gsk_composite_yellow = {"Methanesulfonic acid":"75-75-2",
+                        "Formic acid":"64-18-6",
+                        "Acetic acid (glacial)":"64-19-7",
+                        "1,2-Isopropylidene glycerol":"100-79-8",
+                        "t-Butanol":"75-65-0",
+                        "IMS (ethanol, denatured)":"64-17-5",
+                        "Methanol":"67-56-1"}
+
+gsk_composite_green = {}
+
+gsk_inc_red = {}
+
+gsk_inc_yellow = {}
+
+gsk_inc_green = {}
+
+gsk_voc_red = {}
+
+gsk_voc_yellow = {}
+
+gsk_voc_green = {}
+
+gsk_aqua_red = {}
+
+gsk_aqua_yellow = {}
+
+gsk_aqua_green = {}
+
+gsk_air_red = {}
+
+gsk_air_yellow = {}
+
+gsk_air_green = {}
+
+gsk_health_red = {}
+
+gsk_health_yellow = {}
+
+gsk_health_green = {}
